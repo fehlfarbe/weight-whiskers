025bba4f7ba9cf135a264d64b8aba1ba36174a7c
fehlfarbe <marcus@degenkolbe.eu>
2023-08-03 01:26:54 +0200

    Create react app on buildfs/uploadfs

--- update_fs.py
+++ update_fs.py
@@ -1,9 +1,9 @@
 import os
 Import("env")
 
-def before_upload(source, target, env):
+def before_buildfs(source, target, env):
     print("Building react app...")
     os.system("./create_www.sh")
-    # do some actions
 
-env.AddPreAction("upload", before_upload)
+env.AddPreAction("buildfs", before_buildfs)
+env.AddPreAction("uploadfs", before_buildfs)
